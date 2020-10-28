665f21f23d247d407e32240f00d53c83e07c8bb7
cahoover <cahoover@gmail.com>
2020-10-28 15:26:05 -0700

    minor tweaks

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::statamic('/', 'home', ['load' => '/home']);
-Route::statamic('google-ads', 'google-ads.index', ['load' => '/google-ads']);
-Route::statamic('cookbook', 'cookbook.index', ['load' => '/cookbook']);
-Route::statamic('blog', 'blog.index', ['load' => '/blog']);
+Route::statamic('/', 'home', ['load' => '/pages/home']);
+Route::statamic('google-ads', 'google-ads.index', ['load' => '/pages/google-ads']);
+Route::statamic('cookbook', 'cookbook.index', ['load' => '/pages/cookbook']);
+Route::statamic('blog', 'blog.index');
 Route::statamic('search-results', 'search');
 // Route::permanentRedirect('collections-and-entries', 'collections');
 // Route::permanentRedirect('entries', 'collections#entries');
